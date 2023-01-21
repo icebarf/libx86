7a8b68d24334c7a987979f125e050c5d481f755c
icebarf <sysgrammer@protonmail.com>
2023-01-21 09:40:31 +0530

    minor documentation change + better formatted licensing

--- dict.asm
+++ dict.asm
@@ -1,17 +1,14 @@
-;This file is part of libx86.
-;libx86 is free software: you can redistribute it and/or modify it under the 
-;terms of the GNU Affero General Public License as published by the Free 
-;Software Foundation, either version 3 of the License, or (at your option) 
-;any later version.
-;libx86 is distributed in the hope that it will be useful, but 
-; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-;or FITNESS FOR A PARTICULAR PURPOSE. 
-;See the GNU Affero General Public License for more details.
-;You should have received a copy of the 
-;GNU Affero General Public License along with libx86. 
-;If not, see <https://www.gnu.org/licenses/>. 
-
-
+;;  This file is part of libx86
+;;  libx86 is free software: you can redistribute it and/or modify it 
+;;  under the terms of the GNU General Public License as published by the 
+;;  Free Software Foundation, either version 3 of the License, 
+;;  or (at your option) any later version.
+;;  This program is distributed in the hope that it will be useful, 
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+;;  See the GNU General Public License for more details.
+;;  You should have received a copy of the GNU General Public License 
+;;  along with this program. If not, see <https://www.gnu.org/licenses/>. 
 
 global find_word
 
@@ -20,7 +17,7 @@ extern strcmp
 ; find_word - rdi: pointer to null-terminated `key` string
 ;           - rsi: pointer to last entry in dictionary/list
 ;             returns a pointer to the address of the entry of the key 
-;             if found otherwise, a zero is returned
+;             if not found otherwise, a zero is returned
 find_word:
     xor rax, rax
 .find:
